7b4768a2c0f9e22b3de95c95c8570e6ec8efcd4f
Matej Hamas <matej.hamas@gmail.com>
2017-04-07 22:36:20 +0200

    WIP

--- FE/src/configPage.tsx
+++ FE/src/configPage.tsx
@@ -1,7 +1,41 @@
 import * as React from "react";
 
 export class ConfigPage extends React.Component<{}, {}> {
-    render(){
-        return (<h1>Hi</h1>);
+    public render(){
+        return (
+            <div>
+                {this.renderTable()}
+            </div>
+        );
+    }
+
+    private renderTable() {
+        return (
+            <table className="pt-table pt-striped">
+                <thead>
+                    <th>Input Variable</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Gyro X</td>
+                    </tr>
+                    <tr>
+                        <td>Gyro Y</td>
+                    </tr>
+                    <tr>
+                        <td>Gyro Z</td>
+                    </tr>
+                    <tr>
+                        <td>Accelerometer X</td>
+                    </tr>
+                    <tr>
+                        <td>Accelerometer Y</td>
+                    </tr>
+                    <tr>
+                        <td>Accelerometer Z</td>
+                    </tr>                       
+                </tbody>
+            </table>
+        );
     }
 }
