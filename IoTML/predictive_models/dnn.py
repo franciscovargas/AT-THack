780b00430f9778a58d4055c59bf344b9a637c145
Robert Lacok <robo.lacok@gmail.com>
2017-04-08 00:04:35 +0000

    Boilerplate code

--- IoTML/predictive_models/dnn.py
+++ IoTML/predictive_models/dnn.py
@@ -8,77 +8,44 @@ import urllib
 import tensorflow as tf
 import numpy as np
 
-# Data sets
-IRIS_TRAINING = "iris_training.csv"
-IRIS_TRAINING_URL = "http://download.tensorflow.org/data/iris_training.csv"
 
-IRIS_TEST = "iris_test.csv"
-IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
+class Classifier:
 
-def main():
-    # If the training and test sets aren't stored locally, download them.
-    if not os.path.exists(IRIS_TRAINING):
-        raw = urllib.urlopen(IRIS_TRAINING_URL).read()
-        with open(IRIS_TRAINING, "w") as f:
-            f.write(raw)
+    def __init__(self, n_features, n_classes, hidden_units):
+        # Specify that all features have real-value data
+        feature_columns = [tf.contrib.layers.real_valued_column("", dimension=n_features)]
+        
+        self.classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
+                                                    hidden_units=hidden_units,
+                                                    n_classes=n_classes,
+                                                    model_dir="/tmp/iris_model")
+        pass
 
-    if not os.path.exists(IRIS_TEST):
-        raw = urllib.urlopen(IRIS_TEST_URL).read()
-        with open(IRIS_TEST, "w") as f:
-            f.write(raw)
+    def fit(self, training_file):
+        # Load datasets.
+        training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+                filename=training_file,
+                target_dtype=np.int,
+                features_dtype=np.float32)
 
-    # Load datasets.
-    training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-            filename=IRIS_TRAINING,
-            target_dtype=np.int,
-            features_dtype=np.float32)
-    test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
-            filename=IRIS_TEST,
-            target_dtype=np.int,
-            features_dtype=np.float32)
 
-    # Specify that all features have real-value data
-    feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
 
-    # Build 3 layer DNN with 10, 20, 10 units respectively.
-    classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                                hidden_units=[10, 20, 10],
-                                                n_classes=3,
-                                                model_dir="/tmp/iris_model")
-    # Define the training inputs
-    def get_train_inputs():
-        x = tf.constant(training_set.data)
-        y = tf.constant(training_set.target)
+        # Build 3 layer DNN with 10, 20, 10 units respectively.
+        # Define the training inputs
+        def get_train_inputs():
+            x = tf.constant(training_set.data)
+            y = tf.constant(training_set.target)
 
-        return x, y
+            return x, y
 
-    # Fit model.
-    classifier.fit(input_fn=get_train_inputs, steps=2000)
+        # Fit model.
+        self.classifier.fit(input_fn=get_train_inputs, steps=2000)
 
-    # Define the test inputs
-    def get_test_inputs():
-        x = tf.constant(test_set.data)
-        y = tf.constant(test_set.target)
+    def predict(self, datapoint):
 
-        return x, y
+        # Classify two new flower samples.
+        def new_samples():
+            return np.array(datapoint, dtype=np.float32)
 
-    # Evaluate accuracy.
-    accuracy_score = classifier.evaluate(input_fn=get_test_inputs,
-                                                     steps=1)["accuracy"]
+        return list(classifier.predict(input_fn=new_samples))
 
-    print("\nTest Accuracy: {0:f}\n".format(accuracy_score))
-
-    # Classify two new flower samples.
-    def new_samples():
-        return np.array(
-            [[6.4, 3.2, 4.5, 1.5],
-             [5.8, 3.1, 5.0, 1.7]], dtype=np.float32)
-
-    predictions = list(classifier.predict(input_fn=new_samples))
-
-    print(
-            "New Samples, Class Predictions:        {}\n"
-            .format(predictions))
-    print("hello")
-if __name__ == "__main__":
-        main()
