2d4c4b8424a53655b3bc2bb4f6519c4b9993884b
Matej Hamas <matej.hamas@gmail.com>
2017-04-08 08:16:47 +0200

    magic

--- FE/src/thirdPage.tsx
+++ FE/src/thirdPage.tsx
@@ -4,6 +4,7 @@ import "../res/CNN.png";
 import "../res/LSTM.png";
 import "../res/SLSTM.png";
 
+import { NonIdealState } from "@blueprintjs/core";
 
 import { RxComponent } from "./utils/rxComponent";
 import { ThirdPageModel, ThirdPageState } from "./thirdPageModel";
@@ -13,17 +14,17 @@ export class ThirdPage extends RxComponent<ThirdPageState, ThirdPageModel> {
     public render() {
         
         return (
-            <div className="pt-elevation-1">
+            <div>
                <h2>Choose the model</h2>
                <p className="ui-text">
                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ullamcorper, tellus et accumsan faucibus, justo metus facilisis libero, sit amet mollis enim enim imperdiet libero. Aliquam mollis velit eget est tincidunt mattis. Suspendisse ultrices mollis odio eget aliquet. Duis et dapibus metus, quis dignissim est. Pellentesque a pretium magna. Phasellus interdum mi facilisis orci suscipit, consectetur condimentum purus ultrices. Cras purus ligula, fermentum sed turpis vel, cursus porta ante. Duis rhoncus ipsum eros, eget convallis metus blandit sit amet. Nullam vitae risus facilisis, ultricies diam vitae, feugiat elit. Sed tincidunt lectus nec convallis sodales.
                </p>
-               <Tabs2 id="foo" className="pt-elevation-2" onChange={()=>{return}}>
-                  <Tab2 id="FCNN" title="Fully Connected Neural Network" panel={this.generateFCNNDescription()} />
-                  <Tab2 id="CNN" title="Convolutional Neural Network" panel={this.generateCNNDescription()} />
-                  <Tab2 id="LSTM" title="Long Short Term Memory (LSTM)" panel={this.generateLSTMDescription()} />
-                  <Tab2 id="SLSTM" title="Stacked LSTM" panel={this.generateSLSTMDescription()} />
-                  <Tabs2.Expander />
+               <Tabs2 id="foo" onChange={()=>{return}}>
+                  <Tab2 id="FCNN"  className="strong" title="Fully Connected Neural Network" panel={this.generateFCNNDescription()} />
+                  <Tab2 id="CNN"   className="strong"title="Convolutional Neural Network" panel={this.generateCNNDescription()} />
+                  <Tab2 id="LSTM"  className="strong"title="Long Short Term Memory (LSTM)" panel={this.generateLSTMDescription()} />
+                  <Tab2 id="SLSTM" className="strong"title="Stacked LSTM" panel={this.generateSLSTMDescription()} />
+                  <Tab2 id="Advanced" className="strong" title="Advanced" panel={this.generateSLSTMDescription()} />
               </Tabs2>
           </div>  
         );
@@ -33,6 +34,7 @@ export class ThirdPage extends RxComponent<ThirdPageState, ThirdPageModel> {
       return (
         <div className="description">
           <h3>Fully Connected Neural Network (FC-NN)</h3>
+          <div><NonIdealState title="Missing Info" description="Something missing" /></div>
           <img className="shift-right" src="../res/FCNN.png" />
         </div>
       );
@@ -42,6 +44,7 @@ export class ThirdPage extends RxComponent<ThirdPageState, ThirdPageModel> {
       return (
         <div className="description">
           <h3>Convolutional Neural Network (CNN)</h3>
+          <div><NonIdealState title="Missing Info" description="Something missing" /></div>
           <img className="shift-right" src="../res/CNN.png" />
         </div>
       );
@@ -51,7 +54,7 @@ export class ThirdPage extends RxComponent<ThirdPageState, ThirdPageModel> {
       return (
         <div className="description">
           <h3>Long Short Term Memory (LSTM) Network</h3>
-
+          <div><NonIdealState title="Missing Info" description="Something missing" /></div>
           <img className="shift-right" src="../res/LSTM.png" />
         </div>
       );
@@ -61,7 +64,7 @@ export class ThirdPage extends RxComponent<ThirdPageState, ThirdPageModel> {
       return (
         <div className="description">
           <h3>Stacked Long Short Term Memory (LSTM) Network</h3>
-
+          <div><NonIdealState title="Missing Info" description="Something missing" /></div>
           <img className="shift-right" src="../res/SLSTM.png" />
         </div>
       );
