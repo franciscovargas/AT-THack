7e6a9a214cc9a5f1690b5c8c077437a423894d5e
franciscovargas <vargfran@gmail.com>
2017-04-08 03:00:31 +0100

    setting cnfigs back to me

--- IoTML/predictive_models/mixture_model_alertor.py
+++ IoTML/predictive_models/mixture_model_alertor.py
@@ -16,5 +16,46 @@ def main(test_k=3):
 	print("FITTED")
 
 
+class MixtureModelOptimiser(object):
+
+
+    def __init__(self, X):
+        self.n, self.dim = X.shape
+
+        # hacky bound setting for elbow method
+        self.alpha = 3
+        if self.n > 20:
+            self.alpha = 10
+
+        self.k_max = int(self.n / float(self.alpha))
+
+
+    @staticmethod
+    def agg_dist(clusters, centroids):
+        """
+        Finds intra cluster agregate distance
+        for each cluster and sums them up
+        as an overal measure of dispersion 
+        in the clustering.
+        """
+        euc_dists = list()
+        for i, clut in enumerate(clusters):
+
+            cent = centroids[i]
+            # Matrix mult way:
+            z =  clut - cent
+            # Take advantage of matrix mult in numpy
+            # to avoid slow loops in python
+            # when calculating the cluster inter aggregate distances
+            dist_all = np.diag(np.dot(z, z.T))
+            # this check here checks the method above is correct
+            # Although providing proof is easy.
+            assert dist_all.all() >= 0
+            # sum up and append aggregate distances
+            euc_dists.append(np.sum(dist_all))
+
+        return np.sum(euc_dists)
+
+
 if __name__ == '__main__':
 	main()
